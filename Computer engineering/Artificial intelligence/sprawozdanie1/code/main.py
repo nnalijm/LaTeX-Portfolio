8126d25df34c38642ef8cd7c6a676fdbd092faee
nnalijm <naaaaaaaaaaaad@gmail.com>
2024-02-27 10:28:26 +0100

    update main.py

--- Computer engineering/Artificial intelligence/sprawozdanie1/code/main.py
+++ Computer engineering/Artificial intelligence/sprawozdanie1/code/main.py
@@ -14,7 +14,7 @@ napiwek['maly'] = fuzz.trimf(napiwek.universe, [0, 5, 10])
 napiwek['sredni'] = fuzz.trimf(napiwek.universe, [10, 15, 20])
 napiwek['duzy'] = fuzz.trimf(napiwek.universe, [20, 25, 30])
 
-jedzenie = ctlr.Consequent(np.arange(0, 10.01, 0.01), 'jedzenie')
+jedzenie = ctrl.Consequent(np.arange(0, 10.01, 0.01), 'jedzenie')
 jedzenie['zepsute'] = fuzz.trapmf(jedzenie.universe, [-2, 0, 1, 3])
 jedzenie['wyborne'] = fuzz.trapmf(jedzenie.universe, [7, 9, 10, 12])
 
@@ -51,6 +51,34 @@ ax.set_ylabel('napiwek')
 ax.plot(x,z)
 fig.show()
 
+napiwek_sym.input['obsluga'] = 0
+napiwek_sym.input['jedzenie'] = 0
+napiwek_sym.compute()
+print('Wynik', wynik_sym.output['napiwek'])
+napiwek.view(sim=napiwek_sym)
 
+n_points = 21
+upsampled = np.linspace(0, 10, n_points)
+x, y = np.meshgrid(upsampled, upsampled)
+z = np.zeros_like(x)
+
+for i in range(n_points):
+    for j in range(n_points):
+        napiwek_sym.input['obsluga'] = x[i,j]
+        napiwek_sym.input['jedzenie'] = y[i,j]
+        napiwek_sym.compute()
+        z[i,j] = napiwek_sym.output['napiwek'] 
+
+
+fig = plt.figure(figsize = (8,8))
+ax = fig.add_subplot(111, projection = '3d')
+
+surf = ax.plot_surface(x, y, z, cmap='viridis')
+
+ax.set_xlabel('obsługa')
+ax.set_ylabel('jedzenie')
+ax.set_zlabel('napiwek')
+
+ax.view_init(30, 200)
 
 plt.show()
